f60cfdad909e83a542eb421e818ac9aef6daa5aa
Quinten PADDOCK <qpaddock@c4r12s7.wethinkcode.co.za>
2019-10-15 10:51:56 +0200

    wtf

--- computer.py
+++ computer.py
@@ -3,16 +3,36 @@
 import sys
 import re
 
+def check_power(eqn) :
+    ans = re.findall("X\^([3-9]\d?|\d+\d)", eqn)
+    if len(ans) != 0 :
+        return ans[0]
+    else :
+        return 0
+
+def change_sign(eqn) :
+    search = re.findall("((?:\+|\-)?(?: )?(\d?|\d+?.?\d+?))(?: \* X\^\d)", eqn)
+    print(search)
+    search_neg = [str(float(i[0].replace(" ", "")) * -1) for i in search]
+    print(search_neg)
+    return search_neg
+
 if len(sys.argv) == 2 :
     equation = sys.argv[1]
     equation.upper()
     lhs = equation.split(" = ")[0].upper()
-    rhs = equation.split(" = ")[1]
+    rhs = equation.split(" = ")[1].upper()
     print(lhs)
     print(rhs)
+    
+    rhs_new = change_sign(rhs)
 
-    search = re.findall("((?:\+|-)?(?: )?\d?|\d+?.?\d+?)(?: \* X\^2)", lhs)
-    print(search)
+    if check_power(equation) != 0 :
+        print("Polynomial degree: " + check_power(equation))
+
+    else : 
+        search = re.findall("((?:\+|-)?(?: )?\d?|\d+?.?\d+?)(?: \* X\^1)", lhs)
+        print(search)
 
 else :
     print("No Arguments!")  
