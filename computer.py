38acc86a08fb5a27f295c299268fded9855b7bf0
Quinten PADDOCK <qpaddock@c4r12s7.wethinkcode.co.za>
2019-10-17 11:08:38 +0200

    Just clean up

--- computer.py
+++ computer.py
@@ -7,9 +7,12 @@ def check_power(eqn) :
     ans = re.findall("X\^([3-9]\d?|\d+\d)", eqn)
     if len(ans) != 0 :
         return ans[0]
-    else :
-        ans = re.findall("X\^([2])", eqn)
-        return ans[len(ans) - 1]
+    ans = re.findall("X\^([1-2])", eqn)
+    if len(ans) != 0 :
+        return ans[len(ans)-1]
+    ans = re.findall("X\^([0])", eqn)
+    if len(ans) != 0 :
+        return ans[len(ans)-1]
 
 def simplify(eqn) :
     i = 0
@@ -52,12 +55,45 @@ def change_sign(eqn) :
         j += 1
     return rhs
 
+def highest_power(eqn) :
+    ans = re.findall("X\^([0-9]\d?|\d+\d)", eqn)
+    high = 0
+    for i in ans :
+        i = i.replace(" ", "")
+        if high < float(i) :
+            high = float(i)
+    return(high)
+
+
 def solve(eqn) :
-    search = re.findall("((?:\+|\-)?(?: )?(\d?|\d+?.?\d+?))(?: \* X\^\d)", eqn)
-    c = float(search[0][0].replace(" ", ""))
-    b = float(search[1][0].replace(" ", ""))
-    a = float(search[2][0].replace(" ", ""))
-    print("a = " + str(a) + " b = " + str(b) + " c = " + str(c))
+    power = highest_power(eqn)
+    if power == 0:
+        search = re.findall("((?:\+|\-)?(?: )?(\d?|\d+?.?\d+?))(?: \* X\^\d)", eqn)
+        c = float(search[0][0].replace(" ", ""))
+        print("The solution is: \n" + str(c * -1))
+    if power == 1:
+        search = re.findall("((?:\+|\-)?(?: )?(\d?|\d+?.?\d+?))(?: \* X\^\d)", eqn)
+        c = float(search[0][0].replace(" ", ""))
+        b = float(search[1][0].replace(" ", ""))
+        solution = (c * -1) / (b)
+        print("Solution: " + str(solution))
+    if power == 2:
+        search = re.findall("((?:\+|\-)?(?: )?(\d?|\d+?.?\d+?))(?: \* X\^\d)", eqn)
+        c = float(search[0][0].replace(" ", ""))
+        b = float(search[1][0].replace(" ", ""))
+        a = float(search[2][0].replace(" ", ""))
+        if a == 0:
+            print("Can't solve!")
+            return(0)
+        print("a = " + str(a) + " b = " + str(b) + " c = " + str(c))
+        if (b**2 - (4*a*c)) < 0 :
+            print("Can't solve!")
+            return(0)
+        print("Discriminant is strictly positive, the two solutions are:")
+        solution = (-b - (b**2 - (4*a*c))**0.5)/(2 * a)
+        print(str(solution))
+        solution = (-b + (b**2 - (4*a*c))**0.5)/(2 * a)
+        print(str(solution))
 
     
 
@@ -66,8 +102,6 @@ if len(sys.argv) == 2 :
     equation = equation.upper()
     lhs = equation.split(" = ")[0].upper()
     rhs = equation.split(" = ")[1].upper()
-    print(lhs)
-    print(rhs)
     
     rhs_new = change_sign(rhs)
     equation = simplify(lhs + rhs_new)
@@ -75,9 +109,9 @@ if len(sys.argv) == 2 :
 
     if float(check_power(equation)) >= 3 :
         print("Polynomial degree: " + check_power(equation))
+        print("The polynomial degree is strictly greater than 2, I can't solve.")
 
-    else : 
-        print("im here")
+    else :
         print("Polynomial degree: " + check_power(equation))
         ans = solve(equation)
 
